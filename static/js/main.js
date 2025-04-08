4479429f163a89e21ec2a01abe8896fb75efb7b2
Shadd Watsone <watson.shadd@outlook.com>
2025-04-07 23:48:37 -0400

    Added AlpineJS to make button show or hide toast

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,11 @@
 import '../css/main.css'
 import 'flowbite';
 import htmx from 'htmx.org';
+import Alpine from 'alpinejs';
 
+// Make AlpineJS globally accessible
+window.Alpine = Alpine
+
+Alpine.start()
 // Make HTMX globally accessible
 window.htmx = htmx
