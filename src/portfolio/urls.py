f465deb52b65dfa856395d074fa40e8e82c09893
Shadd Watsone <watson.shadd@outlook.com>
2025-04-12 04:26:57 -0400

    Added header and footer html pages

--- src/portfolio/urls.py
+++ src/portfolio/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home_view
+from .views import home_view, about_view, contact_view, projects_view
 
 urlpatterns = [
     
-    path('', home_view, name='index'),
-    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact'),
+    path('projects/', projects_view, name='projects'),
+    # path('admin/', admin.site.urls),
 ]
--- src/portfolio/views.py
+++ src/portfolio/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 
 def home_view(request, *args, **kwargs):
-    return render(request, 'index.html', {})
+    return render(request, 'home.html', {})
+
+def projects_view(request, *args, **kwargs):
+    return render(request, 'projects.html', {})
+
+def about_view(request, *args, **kwargs):
+    return render(request, 'about.html', {})
+
+def contact_view(request, *args, **kwargs):
+    return render(request, 'contact.html', {})
